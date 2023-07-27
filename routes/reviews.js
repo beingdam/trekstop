77af382dc436cb6fbb4da5da981c410286faf686
Dam <dam17.engineer@gmail.com>
2023-07-27 14:04:13 +0530

    restructuring and adding session

---
+++ routes/reviews.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const router = express.Router({ mergeParams: true });
+const { reviewSchema } = require("../schemas.js");
+const catchAsync = require("../utils/catchAsync");
+const ExpressError = require("../utils/ExpressError");
+const Campground = require("../models/campground");
+const Review = require("../models/review");
+
+// server-side validation with Joi
+const validateReview = (req, res, next) => {
+  const { error } = reviewSchema.validate(req.body);
+  if (error) {
+    const message = error.details.map((el) => el.message).join(",");
+    throw new ExpressError(message, 400);
+  } else {
+    next();
+  }
+};
+
+// adding review
+router.post(
+  "/",
+  validateReview,
+  catchAsync(async (req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    const review = new Review(req.body.review);
+    campground.reviews.push(review);
+    await review.save();
+    await campground.save();
+    req.flash("success", "Created new review!");
+    res.redirect(`/campgrounds/${campground._id}`);
+  })
+);
+
+// deleting a review
+router.delete(
+  "/:reviewId",
+  catchAsync(async (req, res) => {
+    const { id, reviewId } = req.params;
+    await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
+    await Review.findByIdAndDelete(reviewId);
+    req.flash("success", "Successfully deleted the review!");
+    res.redirect(`/campgrounds/${id}`);
+  })
+);
+
+module.exports = router;
