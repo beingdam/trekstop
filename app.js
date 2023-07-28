03c63e73392e99b06e3081c4cd5519405e105a45
Dam <dam17.engineer@gmail.com>
2023-07-28 17:01:59 +0530

    initializing authentication

--- app.js
+++ app.js
@@ -6,8 +6,12 @@ const session = require("express-session");
 const flash = require("connect-flash");
 const ExpressError = require("./utils/ExpressError");
 const methodOverride = require("method-override");
-const campgrounds = require("./routes/campgrouds");
-const reviews = require("./routes/reviews");
+const campgroundRoutes = require("./routes/campgrouds");
+const reviewRoutes = require("./routes/reviews");
+const userRoutes = require("./routes/users");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user");
 
 try {
   mongoose.connect("mongodb://127.0.0.1:27017/yelpcamp");
@@ -46,13 +50,21 @@ const sessionConfig = {
 
 app.use(session(sessionConfig));
 app.use(flash());
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 app.use((req, res, next) => {
   res.locals.success = req.flash("success");
   res.locals.error = req.flash("error");
   next();
 });
-app.use("/campgrounds", campgrounds);
-app.use("/campgrounds/:id/reviews", reviews);
+app.use("/campgrounds", campgroundRoutes);
+app.use("/campgrounds/:id/reviews", reviewRoutes);
+app.use("/", userRoutes);
 
 app.get("/", (req, res) => {
   res.render("home");
